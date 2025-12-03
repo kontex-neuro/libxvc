6ca77b2ceab61797c3ba0a06db8750414bbd7556
Steven Shen <zhexiushen@gmail.com>
2025-12-03 11:08:45 +0800

    Upgrade dep xdaqmetadata to 0.1.1

--- conanfile.py
+++ conanfile.py
@@ -26,7 +26,7 @@ class libxvc(ConanFile):
         self.requires("spdlog/1.13.0")
         self.requires("nlohmann_json/3.11.3")
         self.requires("cpr/1.10.5")
-        self.requires("xdaqmetadata/0.1.0")
+        self.requires("xdaqmetadata/0.1.1")
         self.requires("openssl/3.4.1")
         self.requires("cli11/2.5.0")
 
