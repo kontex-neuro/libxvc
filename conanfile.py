67e51d8ac649d0df64183ee7b9b2bb13cd98f1e7
Steven Shen <zhexiushen@gmail.com>
2026-02-11 17:38:37 +0800

    fix: upgrade xdaqmetadata version

--- conanfile.py
+++ conanfile.py
@@ -27,7 +27,7 @@ class libxvc(ConanFile):
         self.requires("nlohmann_json/3.11.3")
         self.requires("json-schema-validator/2.3.0")
         self.requires("cpr/1.10.5")
-        self.requires("xdaqmetadata/0.1.2")
+        self.requires("xdaqmetadata/0.2.0")
         self.requires("openssl/3.4.1")
         self.requires("cli11/2.5.0")
 
